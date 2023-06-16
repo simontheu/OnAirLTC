965ec5cd7bbab6ed15a5d399feb14d5c9e846909
Simon Unsworth <simon@leobodnar.com>
2023-06-16 10:27:50 +0100

    Fixed update issue

--- LTC_to_UDP_serial.py
+++ LTC_to_UDP_serial.py
@@ -17,7 +17,9 @@ ticks = 0;
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #IP and UDP
 
-serialDevice = serial.Serial('/dev/ttyACM0', timeout=10)
+#serialDevice = serial.Serial('/dev/ttyACM0', timeout=10)
+serialDevice = serial.Serial('/dev/tty.usbmodem11101', timeout=10)
+
 print (serialDevice.name)
 
 tcBytesRead = 0
@@ -62,7 +64,7 @@ def decodeTC(hours, mins, seconds, frames, FPS):
 	lastMins = mins
 	lastSeconds = seconds
 
-	if (invalid != 1) and seconds == 0 and frames == 0: #Send only seconds
+	if (invalid != 1): 
 		lastSeconds = seconds
 		#Send Time Out
 		txt = "LTC_TIME:{:02d}:{:02d}:{:02d}:{:02d}"
